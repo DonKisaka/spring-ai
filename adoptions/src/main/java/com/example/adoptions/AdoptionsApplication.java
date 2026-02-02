e0d9d35dae7837c5cb05d585497f2571540e45a3
DonaldKisaka <davidkisaka4@gmail.com>
2026-02-02 10:00:18 +0300

    chat model

--- adoptions/src/main/java/com/example/adoptions/AdoptionsApplication.java
+++ adoptions/src/main/java/com/example/adoptions/AdoptionsApplication.java
@@ -1,7 +1,12 @@
 package com.example.adoptions;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class AdoptionsApplication {
@@ -9,5 +14,28 @@ public class AdoptionsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AdoptionsApplication.class, args);
 	}
+
+}
+
+@Controller
+@ResponseBody
+class AdoptionController {
+
+    private final ChatClient  ai;
+
+    public AdoptionController(ChatClient.Builder ai) {
+        this.ai = ai.build();
+    }
+
+    @PostMapping("/{user}/inquire")
+    String inquire (
+            @RequestParam String question
+    ) {
+        return this.ai
+                .prompt()
+                .user( question )
+                .call()
+                .content();
+    }
 
 }
